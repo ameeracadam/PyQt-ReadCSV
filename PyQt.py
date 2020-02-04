cd1e1740f16e54db969279fb8e1cdada94fc3c63
ameera <ameera.chumma.adam@gmail.com>
2020-02-04 16:42:39 +0800

    Updated PyQt

--- PyQt.py
+++ PyQt.py
@@ -4,6 +4,8 @@ import os
 import io
 import re
 import pandas as pd
+import numpy as np
+import csv
 from PyQt5 import QtPrintSupport
 
 from PyQt5 import QtCore, QtGui, QtWidgets, QtPrintSupport
@@ -71,8 +73,18 @@ class Example(QMainWindow):
         hashPyAll.triggered.connect(self.hashPy_all)
         hashPyCol.triggered.connect(self.hashPy_col)
 
+        hash_sha1 = QtWidgets.QMenu('sha1 Hashing', self)
+        hash_sha1_All = QtWidgets.QAction('Hash All', self) 
+        hash_sha1_Col = QtWidgets.QAction('Hash Selected Column', self)
+        hash_sha1.addAction(hash_sha1_All)     
+        hash_sha1.addAction(hash_sha1_Col)
+
+        # hash_sha1_All.triggered.connect(self.hash_sha1_all)
+        # hash_sha1_Col.triggered.connect(self.hash_sha1_col)
+
         fileMenu.addMenu(hashPy)
         fileMenu.addMenu(hashReg)
+        fileMenu.addMenu(hash_sha1)
 
         self.setMinimumSize(820,820)
         self.setWindowTitle('PyQy Hash')    
@@ -80,50 +92,56 @@ class Example(QMainWindow):
     
     
     def loadCsv(self, fileName):
-       fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Open CSV",
+        fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Open CSV",
                (QtCore.QDir.homePath()), "CSV (*.csv)")
  
-       if fileName:
-           print(fileName)
-           ff = open(fileName, 'r')
-           mytext = ff.read()
-           ff.close()
-           f = open(fileName, 'r')
-           with f:
-               self.fname = os.path.splitext(str(fileName))[0].split("/")[-1]
-               self.setWindowTitle(self.fname)
-               if mytext.count(';') <= mytext.count('\t'):
-                   reader = csv.reader(f, delimiter = ',')
-                   self.model.clear()
-                   for row in reader:    
-                       items = [QtGui.QStandardItem(field) for field in row]
-                       self.model.appendRow(items)
-                   self.tableView.resizeColumnsToContents()
-                   self.tableView.resizeRowsToContents()
-                   print("File Loaded")
+        if fileName:
+            print(fileName)
+            ff = open(fileName, 'r')
+            mytext = ff.read()
+            ff.close()
+            f = open(fileName, 'r')
+            with f:
+                self.fname = os.path.splitext(str(fileName))[0].split("/")[-1]
+                self.setWindowTitle(self.fname)
+                if mytext.count(';') <= mytext.count('\t'):
+                    reader = csv.reader(f, delimiter = ',')
+                    self.model.clear()
+                    for row in reader:    
+                        items = [QtGui.QStandardItem(field) for field in row]
+                        self.model.appendRow(items)
+                    self.tableView.resizeColumnsToContents()
+                    self.tableView.resizeRowsToContents()
+                    print("File Loaded")
 
     def writeCsv(self, fileName):
        # find empty cells
-       for row in range(self.model.rowCount()):
-           for column in range(self.model.columnCount()):
-               myitem = self.model.item(row,column)
-               if myitem is None:
-                   item = QtGui.QStandardItem("")
-                   self.model.setItem(row, column, item)
-       fileName, _ = QtWidgets.QFileDialog.getSaveFileName(self, "Save File", 
-                       (QtCore.QDir.homePath() + "/" + self.fname + ".csv"),"CSV Files (*.csv)")
-       if fileName:
-           print(fileName)
-           f = open(fileName, 'w', newline = '')
-           with f:
-               writer = csv.writer(f, delimiter = ',')
-               for rowNumber in range(self.model.rowCount()):
-                   fields = [self.model.data(self.model.index(rowNumber, columnNumber),
-                                        QtCore.Qt.DisplayRole)
-                    for columnNumber in range(self.model.columnCount())]
-                   writer.writerow(fields)
-               self.fname = os.path.splitext(str(fileName))[0].split("/")[-1]
-               self.setWindowTitle(self.fname)
+        for row in range(self.model.rowCount()):
+            for column in range(self.model.columnCount()):
+                myitem = self.model.item(row,column)
+                if myitem is None:
+                    item = QtGui.QStandardItem("")
+                    self.model.setItem(row, column, item)
+        fileName, _ = QtWidgets.QFileDialog.getSaveFileName(self, "Save File", 
+                        (QtCore.QDir.homePath() + "/" + self.fname + ".csv"),"CSV Files (*.csv)")
+        if fileName:
+            print(fileName)
+            f = open(fileName, 'w', newline = '')
+            with f:
+                writer = csv.writer(f, delimiter = ',')
+                for rowNumber in range(self.model.rowCount()):
+                    fields = [self.model.data(self.model.index(rowNumber, columnNumber),
+                                         QtCore.Qt.DisplayRole)
+                     for columnNumber in range(self.model.columnCount())]
+                    writer.writerow(fields)
+                self.fname = os.path.splitext(str(fileName))[0].split("/")[-1]
+                self.setWindowTitle(self.fname)
+        
+        # if hashed_indexes(hashed_indexes) is not None:
+        #     hashed_array = np.asarray(hashed_indexes)
+        #     print(type(hashed_array))
+        #     hashed_array.to_csv((QtCore.QDir.homePath() + "/" + self.fname + "indexes.csv"))
+    
 
     def hashRegExp_all(self):
         model = self.model
@@ -149,6 +167,7 @@ class Example(QMainWindow):
                 self.model.item(int(index.row()), int(index.column())).setText(new)
                 #self.tableWidget.setItem(int(index.row()), int(index.column()), new)
             self.tableView.resizeColumnsToContents()
+        
 
     def hashRegExp_col(self):
         model = self.model
@@ -186,6 +205,7 @@ class Example(QMainWindow):
         # numRows = self.tableWidget.rowCount()
         # self.tableWidget.insertRow(numRows)
         pattern = '[a-zA-Z0-9]+'
+        hashed_indexes = []
         for row in range(model.rowCount()):
             data = []
             for column in range(model.columnCount()):
@@ -196,6 +216,7 @@ class Example(QMainWindow):
                 n = i.split(' ')
                 for i in n:
                     if any((len(i)>5) and char.isdigit() for char in i):
+                        hashed_indexes.append(i)
                         i = re.sub(pattern, str(hash(i)), i)
                     new_i.append(i)
                     new = ' '.join(new_i)
@@ -234,7 +255,8 @@ class Example(QMainWindow):
                     self.model.item(int(index.row()), int(index.column())).setText(new)
                     #self.tableWidget.setItem(int(index.row()), int(index.column()), new)
                 self.tableView.resizeColumnsToContents()
-
+    
+    
 
 def stylesheet(self):
        return
